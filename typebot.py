ca0cad5bfafc55ca2a94582facd20556bbf3f535
rashad <rashadphilizaire@gmail.com>
2020-10-18 05:33:06 -0400

    now automatically preps at 3 seconds

--- typebot.py
+++ typebot.py
@@ -3,17 +3,28 @@ from PIL import Image
 
 
 def process():
-    time.sleep(3)
+    found_3 = False #find 3 seconds
+    im = Image.open("3.png")
+    while not found_3:
+        time.sleep(0.6)
+        if pyautogui.locateOnScreen(im, region= (1894, 510, 102, 100)):
+            found_3 = True
     img = pyautogui.screenshot(region=(730, 938, 1246, 256))
     img.save("words.png")
     text = pytesseract.image_to_string(Image.open("words.png"))
-    result = text.replace("\n", " ")
-    if result[0] == "l": #fix errors
-        result = "I" + result[1:]
-    result = result.replace("|", "I")
-    time.sleep(2)
+    result = clean_errors(text)
+    time.sleep(3)
     print(result)
     pyautogui.typewrite(result)
 
+def clean_errors(text):
+    text = text.replace("\n", " ")
+    if text[0] == "l": #fix errors
+        text = "I" + text[1:]
+    text = text.replace("|", "I")
+    text = text.replace("[", "I")
+    return text
 
 process()
+
+
