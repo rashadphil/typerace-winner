595954c82545aa4a4fb4a92c1d2a4de815882c77
rashad <rashadphilizaire@gmail.com>
2020-10-18 06:54:17 -0400

    fixes

--- typebot.py
+++ typebot.py
@@ -4,18 +4,21 @@ from PIL import Image
 
 def process():
     found_3 = False #find 3 seconds
-    im = Image.open("3.png")
+    three_img = Image.open("3.png")
+    print("Searching for ':03'")
     while not found_3:
-        time.sleep(0.6)
-        if pyautogui.locateOnScreen(im, region= (1894, 510, 102, 100)):
+        if pyautogui.locateOnScreen(three_img):
             found_3 = True
-    img = pyautogui.screenshot(region=(730, 938, 1246, 256))
+            print("FOUND':03'")
+
+    img = pyautogui.screenshot(region=(730, 960, 1246, 300))
     img.save("words.png")
     text = pytesseract.image_to_string(Image.open("words.png"))
     result = clean_errors(text)
-    time.sleep(3)
     print(result)
-    pyautogui.typewrite(result)
+    time.sleep(2.5)
+    pyautogui.press(result[0].upper()) #because of glitch sometimes
+    pyautogui.typewrite(result[1:])
 
 def clean_errors(text):
     text = text.replace("\n", " ")
@@ -23,6 +26,12 @@ def clean_errors(text):
         text = "I" + text[1:]
     text = text.replace("|", "I")
     text = text.replace("[", "I")
+
+    if text[1].isalpha() and text[1].upper() == text[1]: #means OCR detected the cursor as an I
+        text = text[1:]
+
+    end_idx = text.find("change display format")
+    text = text[:end_idx].rstrip()
     return text
 
 process()
